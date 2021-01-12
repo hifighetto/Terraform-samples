ede6cef8a78e3c115f0408947cfdcc1a9265825b
Hifi Ghetto <hifighetto@gmail.com>
2021-01-12 15:24:56 -0800

    Updated vpc\backend.tf
    Updated `vpc\backend.tf` for support for terraform 0.14.x
    Mainly this was changing how the provider resource was included

--- projects/examples/vpc/backend.tf
+++ projects/examples/vpc/backend.tf
@@ -1,20 +1,24 @@
 terraform {
   backend "s3" {
-    encrypt              = "true"
-    bucket               = "ChangeMe"
+    encrypt = "true"
+    bucket  = "ChangeMe"
     # Change to your Bucket 
-    key                  = "terraform/states/vpc_main/"
-    region               = "us-west-2"
+    key    = "terraform/states/vpc.tfstate"
+    region = "us-west-2"
     # Change to your dynamo-db table 
-    dynamodb_table       = "ChangeMe"
+    dynamodb_table = "ChangeMe"
     # Uncomment below and add your workspace prefix key
     # workspace_key_prefix = "MyWorkspace"
   }
 
-  required_version = ">=0.12.0"
+  required_providers {
+    aws = {
+      version = "3.22.0"
+      source  = "hashicorp/aws"
+    }
+  }
 }
 
 provider "aws" {
-  version = "~> 3.1.0"
-  region  = "us-west-2"
+  region = "us-west-2"
 }
